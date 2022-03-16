687e24e4e6cdaa768fefa5be9fef84bd6baa0ff8
CHEY777 <88870986+CHEY777@users.noreply.github.com>
2022-03-17 05:22:31 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -1,60 +1,58 @@
 /** @format */
 
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import { MessageType } from "@adiwajshing/baileys";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "broadcast",
-			description:
-				"Will make a broadcast for groups where the bot is in. Can be used to make announcements.",
-			aliases: ["bcast", "announcement", "bc"],
-			category: "dev",
-			dm: true,
-			usage: `${client.config.prefix}bc`,
-			modsOnly: true,
-			baseXp: 0,
-		});
-	}
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "broadcast",
+      description:
+        "Will make a broadcast for groups where the bot is in. Can be used to make announcements.",
+      aliases: ["bcast", "announcement", "bc"],
+      category: "dev",
+      dm: true,
+      usage: `${client.config.prefix}bc`,
+      modsOnly: true,
+      baseXp: 0,
+    });
+  }
 
-	run = async (
-		M: ISimplifiedMessage,
-		{ joined }: IParsedArgs
-	): Promise<void> => {
-		if (!joined)
-			return void (await M.reply(`Please provide the Broadcast Message.`));
-		const term = joined.trim();
-		const gifs = [
-			"https://c.tenor.com/qGBj-9nx7rsAAAPo/what-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/mMRNByRo8XMAAAPo/nakano-yotsuba-disturbance.mp4",
-			"https://c.tenor.com/yQ_zLRi6zUkAAAPo/yotsuba-nakano.mp4",
-			"https://c.tenor.com/DpLoI6rRq4YAAAPo/nakano-yotsuba-ribbon.mp4",
-			"https://c.tenor.com/dEnq15fNv6kAAAPo/aaaa-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/i1fRMMMvu38AAAPo/yotsuba-nakano-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/l2F9LrkJEKYAAAPo/yotsuba-nakano.mp4",
+  run = async (
+    M: ISimplifiedMessage,
+    { joined }: IParsedArgs
+  ): Promise<void> => {
+    if (!joined)
+      return void (await M.reply(`Please provide the Broadcast Message.`));
+    const term = joined.trim();
+    const gifs = [
+      "https://telegra.ph/file/31cf94618271ff9d29d8e.mp4",
+			"https://telegra.ph/file/17ecc2eece63a4d8bcd51.mp4",
+			"https://telegra.ph/file/621c985f4ca375cfd7df7.mp4",
+			"https://telegra.ph/file/3645407aa72f7c2001ad6.mp4",
+                        "https://telegra.ph/file/0fde71c91c0e8813b754f.mp4",
+                        "https://telegra.ph/file/7afc21a42c7aab0a1c07d.mp4",
+
 		];
-		const selected = gifs[Math.floor(Math.random() * gifs.length)];
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const chats: any = this.client.chats
-			.all()
-			.filter((v) => !v.read_only && !v.archive)
-			.map((v) => v.jid)
-			.map((jids) => (jids.includes("g.us") ? jids : null))
-			.filter((v) => v);
-		for (let i = 0; i < chats.length; i++) {
-			const text = `*⚡「YOTSUBA BROADCAST」⚡*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
-			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
-				mimetype: Mimetype.gif,
-				caption: `${text}`,
-				contextInfo: {
-					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
+    const selected = gifs[Math.floor(Math.random() * gifs.length)];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const chats: any = this.client.chats
+      .all()
+      .filter((v) => !v.read_only && !v.archive)
+      .map((v) => v.jid)
+      .map((jids) => (jids.includes("g.us") ? jids : null))
+      .filter((v) => v);
+    for (let i = 0; i < chats.length; i++) {
+      const text = `🎆_*MARIN*_🎆\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+      this.client.sendMessage(chats[i], { url: selected }, MessageType.image, {
+        caption: `${text}`,
+        contextInfo: {	mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
 				},
-			});
-		}
-		await M.reply(`✅ Broadcast Message sent to *${chats.length} groups*.`);
-	};
+      });
+    }
+    await M.reply(`✅ Broadcast Message sent to *${chats.length} groups*.`);
+  };
 }
