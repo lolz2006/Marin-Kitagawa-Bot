a5ece9c7faf3dd33a2e14b87c92d238828600ad0
CHEY777 <88870986+CHEY777@users.noreply.github.com>
2022-03-17 06:14:50 +0530

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -1,35 +1,63 @@
 import { MessageType, Mimetype } from '@adiwajshing/baileys'
+
 import MessageHandler from '../../Handlers/MessageHandler'
+
 import BaseCommand from '../../lib/BaseCommand'
+
 import WAClient from '../../lib/WAClient'
+
 import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
+
     constructor(client: WAClient, handler: MessageHandler) {
+
         super(client, handler, {
+
             command: 'support',
+
             aliases: ['support'],
+
             description: 'Gets the support group links',
+
             category: 'general',
+
             usage: `${client.config.prefix}Support`,
+
             baseXp: 10
+
         })
+
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
+
         (await this.client.sendMessage(
+
         M.sender.jid,
-        `    ♥️MY MASTER GROUP♥️\n\n*🎇YOTSUBA support:🎇* *https://chat.whatsapp.com/JkxmxZ593N16JYaiJ4Sx09*\n\n*💫INFINITY BOT FUN GROUP💫*:*https://chat.whatsapp.com/F0lsoalrK7yInLXShKdWJ6*`,
+
+        `    ♥️MY DARLING SENPAI ID♥️\n\n*Instagram * *https://instagram.com/its_chey_7*\n\n*💫 NSFW GROUP💫*:*https://chat.whatsapp.com/GIMBtutJAWYBVWbWOkaOfp*`,
+
            MessageType.text
+
         ))
+
         const n = [
-            'https://c.tenor.com/DnRViqnLNsUAAAPo/gotoubun-no-hanayome-anime.mp4'
+
+            'https://telegra.ph/file/38a9538ae1f2387235caf.mp4'
+
         ]
+
         let beckylynch = n[Math.floor(Math.random() * n.length)]
+
         return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
+
             mimetype: Mimetype.gif,
-            caption: `Sent you the support Link in personal message \n` }
+
+            caption: `Sent you the support Link in personal message #checkdm \n` }
+
         )
 
         }
+
 }
