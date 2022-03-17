7cb581f3b5d80ce33cd025b54ecf89a7b7262da3
CHEY777 <88870986+CHEY777@users.noreply.github.com>
2022-03-17 05:50:22 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -5,7 +5,6 @@ import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
 import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import request from '../../lib/request'
 
-
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
@@ -13,76 +12,333 @@ export default class Command extends BaseCommand {
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help (command_name)`,
-            aliases: ['h']
+            aliases: ['h', 'list', 'menu']
         })
     }
 
-    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
-            const n = [
-            'https://telegra.ph/file/cc211d2b195ccbef82bff.mp4'
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            'https://telegra.ph/file/36783a395d86adbf9fcd4.mp4','https://telegra.ph/file/fe3325fb5fa3da1de4c04.mp4','https://telegra.ph/file/29a367457d46721b59528.mp4','https://telegra.ph/file/02c8cb90510ad6fd1e02d.mp4','https://telegra.ph/file/0e633bf979e25ef592c10.mp4','https://telegra.ph/file/0fde71c91c0e8813b754f.mp4'
         ]
-        let chitoge = n[Math.floor(Math.random() * n.length)]
-	if (!parsedArgs.joined) {
-			const commands = this.handler.commands.keys();
-			const categories: { [key: string]: ICommand[] } = {};
-			for (const command of commands) {
-				const info = this.handler.commands.get(command);
-				if (!command) continue;
-				if (!info?.config?.category || info.config.category === "dev") continue;
-				if (
-					!info?.config?.category ||
-					(info.config.category === "nsfw" &&
-						!(await this.client.getGroupData(M.from)).nsfw)
-				)
-					continue;
-				if (Object.keys(categories).includes(info.config.category))
-					categories[info.config.category].push(info);
-				else {
-					categories[info.config.category] = [];
-					categories[info.config.category].push(info);
-				}
-			}
-            let text = `
-╭─「(づ￣ 3￣)づ」
-│⋊ 𝕌𝕤𝕖𝕣: *${M.sender.username}*
-│⋊ ℕ𝕒𝕞𝕖: Yᴏᴛsᴜʙᴀ
-│⋊ ℙ𝕣𝕖𝕗𝕚𝕩: ${this.client.config.prefix}
-│⋊ 𝕆𝕨𝕟𝕖𝕣: *${this.client.config.prefix}mod*
-│⋊ 𝕆𝕗𝕗𝕚𝕔𝕚𝕒𝕝 𝔾𝕣𝕠𝕦𝕡: http://gg.gg/Piku-and-yotsuba-support
-╰────────────┈平和                            \n\n`
-            const keys = Object.keys(categories)
-            for (const key of keys)
-                text += `*『 ${this.client.util.capitalize(
-					key
-	         )} 』*\n❐ \`\`\`${categories[key]
-                    .map((command) => command.config?.command)
-                    .join(', ')}\`\`\`\n\n`
-            return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {quoted:M.WAMessage,
+        let well = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `${text} 
- ──❅┈[ 𝒀𝒐𝒕𝒔𝒖𝒃𝒂 𝑩𝒐𝒕 ]┈❅───
-┌────────────┈❅
-│   🧨 YᴏᴛsUʙᴀ
-│   ©️ ՏYᑎTᕼᗴՏIᘔᗴᗪ IᑎᖴIᑎITY
-└────────────┈⁂
-❅┈[𝐇𝐚𝐯𝐞 𝐆𝐫𝐞𝐚𝐭 𝐃𝐚𝐲]┈❅
-🎗 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
-            )
-        }
-        const key = parsedArgs.joined.toLowerCase()
-        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
-        if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
-        const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
-        M.reply(
-            `🎈 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
-                state ? 'Disabled' : 'Available'
-            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
-                command.config.aliases
-                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
-                    : ''
-            }\n🎐 *Group Only:* ${this.client.util.capitalize(
-                JSON.stringify(!command.config.dm ?? true)
-            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
+            caption: `Konnichiwa👋 ɪ ᴀᴍ 𝓜𝓪𝓻𝓲𝓷💖🖤!
+
+ᴛʏᴘᴇ " ${this.client.config.prefix}ᴏᴡɴᴇʀ " ᴛᴏ ᴄᴏɴᴛᴀᴄᴛ ᴍʏ ᴏᴡɴᴇʀ!
+
+ᴍʏ ᴘʀᴇғʀɪx ${this.client.config.prefix}
+
+🧾 Here are the listed commands...
+────────────────
+-G E N E R A L 
+${this.client.config.prefix}Ｈelp1 [${this.client.config.prefix}h1]
+────────────────
+〽️~ᴀᴅᴍɪɴs
+💠Description: Tags all Admins 
+🧧Usage: ${this.client.config.prefix}Admins
+
+〽️~ᴇᴠᴇʀʏᴏɴᴇ
+💠Description:
+🧧Usage: ${this.client.config.prefix}Everyone
+
+〽️~ᴍᴏᴅs
+💠Description: Displays the Moderators' contact info
+🧧Usage: ${this.client.config.prefix}Mods
+
+〽️~ᴘʀᴏғɪʟᴇ
+💠Description: Displays user-profile 
+🧧Usage: ${this.client.config.prefix}Profile
+
+〽️~ʀᴀɴᴋ
+💠Description: Displays user-rank
+🧧Usage: ${this.client.config.prefix}Rank
+
+〽️~xᴘ
+💠Description: Displays User's Xp ♨
+🧧Usage: ${this.client.config.prefix}Xp
+
+〽️~ɪɴᴠɪᴛᴇʟɪɴᴋ
+💠Description: Get the group invite link
+🧧Usage: ${this.client.config.prefix}Invitelink
+
+〽️~ᴅᴇʟᴇᴛᴇ
+💠Description: Deletes the quoted Message
+🧧Usage: ${this.client.config.prefix}Delete
+
+〽️~ʀᴇᴛʀɪᴇᴠᴇ
+💠Description: Well....
+🧧Usage: ${this.client.config.prefix}Retrieve
+
+〽️~ʜɪ
+💠Description: Say hi to the bot
+🧧Usage: ${this.client.config.prefix}Hi
+
+〽️~marin
+💠Description: Displays the info
+🧧Usage: ${this.client.config.prefix}marin
+
+〽️~info
+💠Description: Will display the info of the bot
+🧧Usage: ${this.client.config.prefix}info
+────────────────
+-W E E B S 
+${this.client.config.prefix}Ｈelp2 [${this.client.config.prefix}h2]
+────────────────
+🧧 ${this.client.config.prefix}ᴀɴɪᴍᴇ
+🧧 ${this.client.config.prefix}ᴀɴɪᴍᴇQᴜᴏᴛᴇ
+🧧 ${this.client.config.prefix}ᴀɴɪᴍᴇᴄʜᴀʀ
+🧧 ${this.client.config.prefix}ɢᴇɴꜱʜɪɴᴄʜᴀʀᴀᴄᴛᴇ
+🧧 ${this.client.config.prefix}ʜᴜꜱʙᴀɴᴅᴏ
+🧧 ${this.client.config.prefix}ʟᴏʟɪ
+🧧 ${this.client.config.prefix}ᴍᴀɴɢᴀ
+🧧 ${this.client.config.prefix}ᴘᴏᴋᴇᴍᴏɴ
+🧧 ${this.client.config.prefix}ʀᴘᴀᴘᴇʀ
+🧧 ${this.client.config.prefix}ᴠᴛᴜʙᴇʀ
+🧧 ${this.client.config.prefix}ᴡᴀɪꜰᴜ
+🧧 ${this.client.config.prefix}ᴀᴍᴇᴍᴇ
+🧧 ${this.client.config.prefix}ᴄʜᴀʀᴀᴄᴛᴇʀ
+🧧 ${this.client.config.prefix}ᴄʀᴏꜱꜱᴘʟᴀʏ
+🧧 ${this.client.config.prefix}ʜᴀɪɢᴜꜱʜᴀ
+🧧 ${this.client.config.prefix}ʀᴇᴄᴏᴍᴍᴇɴᴅ
+🧧 ${this.client.config.prefix}ꜱᴀᴜᴄᴇ
+🧧 ${this.client.config.prefix}ᴡᴀʟʟᴘᴀᴘᴇʀ
+🧧 ${this.client.config.prefix}rpaper
+🧧 ${this.client.config.prefix}seggs [tag user]
+────────────────
+-F U N 
+${this.client.config.prefix}Ｈelp3 [${this.client.config.prefix}h3]
+────────────────
+〽️~Qᴜᴏᴛᴇ
+💠Discription: random quote
+🧧Usage: ${this.client.config.prefix}Quote
+
+〽️~ʀᴇᴀᴄᴛ
+💠Discription: Let's React
+🧧Usage: ${this.client.config.prefix}React
+
+〽️~ᴛʀɪɢɢᴇʀ
+💠Discription: Sends the triggered version of you
+🧧Usage: ${this.client.config.prefix}Trigger
+
+〽️~ᴛʀᴜᴛʜ
+💠Discription: Gives you random questions
+🧧Usage: ${this.client.config.prefix}Truth
+
+〽️~ᴄʜᴀᴛ
+💠Discription: Chat with the Bot in group
+🧧Usage: ${this.client.config.prefix}Chat
+
+〽️~ᴅᴀʀᴇ
+💠Discription: Gives you random task
+🧧Usage: ${this.client.config.prefix}Dare
+
+〽️~ꜰᴀᴄᴛ
+💠Discription: sends a random fact for you
+🧧Usage: ${this.client.config.prefix}Fact
+
+〽️~ꜱᴀꜰᴇᴊᴏᴋᴇ
+💠Discription: Will send you random safe joke
+🧧Usage: ${this.client.config.prefix}SafeJoke
+
+〽️~sʜɪᴘ
+💠Discription: Ship 💖 People
+🧧Usage: ${this.client.config.prefix}Ship
+
+〽️~ᴊᴀɪʟ
+💠Discription: Who wanna go to jail for being horny?
+🧧Usage: ${this.client.config.prefix}Jail
+
+〽️~ᴡᴀɴᴛᴇᴅ
+💠Discription: Well... make someone wanted
+🧧Usage: ${this.client.config.prefix}Wanted
+
+〽️~ʀɪᴘ
+💠Discription: RIP
+🧧Usage: ${this.client.config.prefix}Rip
+
+〽️~ᴛʀᴀsʜ
+💠Discription: Make someone a trash
+🧧Usage: ${this.client.config.prefix}Trash
+
+〽️~ppcouple
+💠Discription: Sends pair dp
+🧧Usage: ${this.client.config.prefix}ppcouple
+
+〽️~chess
+💠Discription: chesssssssssssssss
+🧧Usage:${this.client.config.prefix}chess
+────────────────
+-M E D I A 
+${this.client.config.prefix}Ｈelp4 [${this.client.config.prefix}h4]
+────────────────
+〽️~ɪɢ
+💠Discription: Get the info of a user from ig
+🧧Usage: ${this.client.config.prefix}Ig
+
+〽️~ᴘʟᴀʏ
+💠Discription: 🎵 play a song with just search term!
+🧧Usage: ${this.client.config.prefix}Play
+
+〽️~ꜱᴘᴏᴛɪꜰʏ
+💠Discription: Downloads given spotify track and sends it as Audio
+🧧Usage: ${this.client.config.prefix}Spotify
+
+〽️~ᴛᴀᴋᴇ
+💠Discription: steals the sticker for you
+🧧Usage: ${this.client.config.prefix}Take
+
+〽️~ʏᴛᴀᴜᴅɪᴏ
+💠Discription: Downloads given YT Video and sends it as Audio
+🧧Usage: ${this.client.config.prefix}Ytaudio
+
+〽️~ʏᴛꜱᴇᴀʀᴄʜ
+💠Discription: Searches YT
+🧧Usage: ${this.client.config.prefix}Ytsearch
+
+〽️~ʏᴛᴠɪᴅᴇᴏ
+💠Discription: Downloads given YT Video
+🧧Usage: ${this.client.config.prefix}Ytvideo
+
+〽️~ɢᴏᴏɢʟᴇ
+💠Discription: Search on the web
+🧧Usage: ${this.client.config.prefix}Google
+
+〽️~ʟʏʀɪᴄꜱ
+💠Discription: Gives you the lyrics of the given song
+🧧Usage: ${this.client.config.prefix}Lyrics
+
+〽️~ᴘɪɴᴛᴇʀᴇꜱᴛ
+💠Discription: Search wallpaper from pinterest.com
+🧧Usage: ${this.client.config.prefix}pinterest
+
+〽️~karaoke
+💠Discription: Gives you karaoke song playable on WhatsApp
+🧧Usage:${this.client.config.prefix}karaoke [term]
+
+〽️~iguser
+💠Discription: Get the info of a user from ig
+🧧Usage:${this.client.config.prefix}iguser [name]
+────────────────
+-U T I L S 
+${this.client.config.prefix}Help5 [${this.client.config.prefix}h5]
+────────────────
+🏮 ${this.client.config.prefix}ʙʟᴜʀ
+🏮 ${this.client.config.prefix}ᴋɪᴛᴛᴇɴ
+🏮 ${this.client.config.prefix}ꜱᴛɪᴄᴋᴇʀ
+🏮 ${this.client.config.prefix}ꜱᴜʙʀᴇᴅ
+🏮 ${this.client.config.prefix}ɢᴇᴛɢɪꜰ
+🏮 ${this.client.config.prefix}ꜱᴄʀᴇᴇɴꜱʜᴏᴛ
+🏮 ${this.client.config.prefix}ꜱᴛᴇᴀʟ
+🏮 ${this.client.config.prefix}ᴛʀᴀɴꜱʟᴀᴛᴇ
+🏮 ${this.client.config.prefix}ᴡɪᴋɪᴘᴇᴅɪᴀ
+🏮 ${this.client.config.prefix}ᴄɪʀᴄʟᴇ
+🏮 ${this.client.config.prefix}shorturl [Your url]
+🏮 ${this.client.config.prefix}toimg [(tag)[sticker]]
+🏮 ${this.client.config.prefix}chey
+🏮 ${this.client.config.prefix}doge
+────────────────
+-M O D E R A T I O N 
+${this.client.config.prefix}Ｈelp6 [${this.client.config.prefix}h6]
+────────────────
+〽️~ᴀᴄᴛɪᴠᴀᴛᴇ
+💠Description: activate certain features on group-chats
+🧧Usage: ${this.client.config.prefix}Activate
+
+〽️~ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇ
+💠Description: deactivate certain features on group-chats
+🧧Usage: ${this.client.config.prefix}Deactivate
+
+〽️~ᴅᴇᴍᴏᴛᴇ
+💠Discription: deactivate certain features on group-chats
+🧧Usage: ${this.client.config.prefix}Demote
+
+〽️~ɢʀᴏᴜᴘᴄʜᴀɴɢᴇ
+💠Description: Updates the Group Subject or Description
+🧧Usage: ${this.client.config.prefix}Groupchange
+
+〽️~ᴘʀᴏᴍᴏᴛᴇ
+💠Discription: promotes the mentioned users
+🧧Usage: ${this.client.config.prefix}promote
+
+〽️~ᴘᴜʀɢᴇ
+💠Discription: Removes all group members
+🧧Usage: ${this.client.config.prefix}Purge
+
+〽️~ʀᴇᴍᴏᴠᴇ
+💠Discription: demotes the mentioned users
+🧧Usage: ${this.client.config.prefix}Remove
+
+〽️~ᴄʟᴏꜱᴇ
+💠Discription: Close the group for all participants. Only Admins can message
+🧧Usage: ${this.client.config.prefix}
+
+〽️~ᴏᴘᴇɴ
+💠Discription: Opens the group for all participants
+🧧Usage: ${this.client.config.prefix}Open
+
+〽️~ʀᴇᴠᴏᴋᴇ
+💠Discription: Revokes the group link
+🧧Usage: ${this.client.config.prefix}Revoke
+
+〽️~ᴘᴘᴄᴏᴜᴘʟᴇ
+💠Discription: send random ppcouple image
+🧧Usage: ${this.client.config.prefix}ppcouple
+────────────────
+-E D U C A T I V E 
+${this.client.config.prefix}Ｈelp7 [${this.client.config.prefix}h7]
+────────────────
+〽️~ᴄᴀʟᴄᴜʟᴀᴛᴏʀ
+💠Description: Calculates the given value
+🧧Usage: ${this.client.config.prefix}calculator
+
+〽️~ᴄᴏᴠɪᴅ
+💠Description: get the covid-19 info of the current place
+🧧Usage: ${this.client.config.prefix}covid
+
+〽️~ᴅᴇꜰɪɴᴇ
+💠Description: Gives you the defination of the given word
+🧧Usage: ${this.client.config.prefix}Define
+
+〽️~ᴇʟᴇᴍᴇɴᴛꜱ
+💠Description: get the info of the chemical element
+🧧Usage: ${this.client.config.prefix}Elements
+ 
+〽️~ɢɪᴛʜᴜʙ
+💠Description: Get github information about a user/repo
+🧧Usage: ${this.client.config.prefix}GitHub
+
+〽️~ᴜʀʙᴀɴᴅɪᴄᴛɪᴏɴᴀʀʏ
+💠Description: Gives you the definition of the given word
+🧧Usage: ${this.client.config.prefix}Urbandictionary
+
+〽️~ᴡᴇᴀᴛʜᴇʀ
+💠Description:
+🧧Usage: ${this.client.config.prefix}Weather
+────────────────
+-N S F W 🔞
+${this.client.config.prefix}Help8 [${this.client.config.prefix}h8]
+────────────────
+🍒 ${this.client.config.prefix}ᴀɴᴀʟ
+🍒 ${this.client.config.prefix}ʙʟᴏᴡᴊᴏʙ
+🍒 ${this.client.config.prefix}ɴꜱꜰᴡᴅᴏᴜᴊɪɴ
+🍒 ${this.client.config.prefix}ɴꜱꜰᴡʟᴏʟɪ
+🍒 ${this.client.config.prefix}ɴꜱꜰᴡᴘᴀᴘᴇʀ
+🍒 ${this.client.config.prefix}ᴛʜɪɢʜꜱ
+🍒 ${this.client.config.prefix}ʜᴇɴᴛᴀɪ
+🍒 ${this.client.config.prefix}ᴍᴀɪᴅ
+🍒 ${this.client.config.prefix}ᴍᴀꜱᴛᴜʀʙᴀᴛɪᴏɴ
+🍒 ${this.client.config.prefix}ɴᴄᴏᴅᴇ
+🍒 ${this.client.config.prefix}ɴꜱꜰᴡ-ᴡᴀɪꜰᴜ
+🍒 ${this.client.config.prefix}ɴꜱꜰᴡᴊᴏᴋᴇ
+🍒 ${this.client.config.prefix}ɴꜱꜰᴡ-ɴᴇᴋᴏ
+🍒 ${this.client.config.prefix}ᴘᴜꜱꜱʏ
+🍒 ${this.client.config.prefix}ᴛʀᴀᴘ
+💦 If You Want More Stuff Type ${this.client.config.prefix}h8
+───────────────
+┌────────────┈
+│  🤍マリン🤍
+└────────────┈` }
         )
     }
 }
