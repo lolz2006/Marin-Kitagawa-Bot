f6d4460353db17899cf17b661382588f9c18518a
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-29 18:07:10 +0530

    Create hello.ts

---
+++ src/commands/General/hello.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "hi",
+			description: "Generally used to check if bot is Up",
+			category: "general",
+			usage: .customPrefix = /Hello|Heya|hlo/i,
+			baseXp: 10,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			'https://file.io/IRBw8kseK4jM'
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.audio,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.audio,
+				caption: `hello senpai ❣️. \n`,
+			}
+		);
+	};
+}
