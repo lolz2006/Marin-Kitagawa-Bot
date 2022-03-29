bbb4575fa2b2887888f06edb204988902eac2096
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-29 17:29:14 +0530

    Update Panties.ts

--- src/commands/Nsfw/Panties.ts
+++ src/commands/Nsfw/Panties.ts
@@ -38,7 +38,7 @@ export default class Command extends BaseCommand {
 					MessageType.image,
 					undefined,
 					undefined,
-					`🌟\n`,
+					`you like my panties senpai 😘\n`,
 					undefined
 				).catch((e) => {
 					console.log(
