02a11862aa2ff6d7d28507de3593567f7368b728
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-04-07 06:29:57 +0530

    Create alive.ts

---
+++ src/commands/General/alive.ts
@@ -0,0 +1,35 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "alive",
+                        aliases: ["Jinda?", "oye", "kahaho"],
+			description: "Generally used to check if bot is Up",
+			category: "general",
+			usage: `${client.config.prefix}alive`,
+			baseXp: 10,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://telegra.ph/file/5ab4ac77b1b92a97b6fca.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `Hey🍭 hey🍭 , senpai🥰, \n\n I am always active 😌. ✨ \n`,
+			}
+		);
+	};
+}
