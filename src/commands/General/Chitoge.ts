d10039a139eeb6809132dc2b8d672adcc7d8872d
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-16 11:56:48 +0530

    Update Chitoge.ts

--- src/commands/General/Chitoge.ts
+++ src/commands/General/Chitoge.ts
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `💖 *MARIN* 💖\n\n🍀 *Description: A WhatsApp Bot With Rich NSFW features based on chitoge.*\n\n🌐 *OFFICIAL BOT URL: https://github.com/ShineiIchijo/Chitoge* \n\n 📒 *Guide: https://github.com/ShineiIchijo/Chitoge-Guides* \n\n 🔮 *BOT URL:https://github.com/Chey-san/Marin-Kitagawa-Bot* \n`,
+				caption: `💖 *MARIN* 💖\n\n🍀 *Description: A WhatsApp Bot With Rich NSFW features based on chitoge.*\n\n🌐 *OFFICIAL BOT URL: https://github.com/akumasugi/Marin-Kitagawa-Bot* \n\n 📒 *Guide: https://github.com/akumasugi/Marin-Kitagawa-Bot* \n\n 🔮 *BOT URL:https://github.com/akumasugi/Marin-Kitagawa-Bot* \n`,
 			}
 		);
 	};
