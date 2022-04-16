09a6dfd708f59465293621be4324365393048a74
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-16 11:29:47 +0530

    Update Chey.ts

--- src/commands/General/Chey.ts
+++ src/commands/General/Chey.ts
@@ -8,35 +8,32 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'chey',
-            description: 'Displays info about chey.',
+            command: 'akuma',
+            description: 'Displays info about akuma.',
             category: 'general',
-            usage: `${client.config.prefix}chey`
+            usage: `${client.config.prefix}akuma`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/49d7f2ea6bf7d9c7938ab.mp4','https://telegra.ph/file/76dfbe1b38a29a999e454.mp4','https://telegra.ph/file/bd71998ba0dcc50e7df1f.mp4'
+            'https://c.tenor.com/LXLRCmwR9KIAAAPo/kitagawa-marin-marin-kitagawa.mp4','https://c.tenor.com/dKz5wKx5YN0AAAPo/sono-bisque-doll-wa-koi-wo-suru-marin-kitagawa.mp4','https://c.tenor.com/uN_NusqVHCgAAAPo/marin-kitagawa-dress-up.mp4'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `Hey I'm CHEY, a learner/student & an upcoming developer in the future.
+            caption: `💖𝙃𝙀𝙇𝙇𝙊!🥰I'm 𝘼𝙆𝙐𝙈𝘼 an ordinary guy who loves watching anime & play games🎮. I'm a BCA Student🤍relationship with 𝙕𝙀𝙍𝙊❤️.
             
-📫𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
-Wa.me/918373017400
+🍀𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
+Wa.me/917892202052
 
-⭕𝙂𝙞𝙩𝙝𝙪𝙗;
-https://github.com/Chey-san
+🍀𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆;
+https://facebook.com/groups/600441174428472/
 
-📮𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢;
-https://instagram.com/its_chey_7
+🍀𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈;
+https://www.instagram.com/akuma__24/?hl=en
 
-🚀𝘿𝙞𝙨𝙘𝙤𝙧𝙙;
-｟𝖢𝗈𝗆𝗂𝗇𝗀 𝖲𝗈𝗈𝗇｠
-
-⪼𝖲𝖾𝖾 𝗒𝖺𝗁 have a great day😉✨` }
+⪼𝖲𝖾𝖾 𝗒𝖺𝗁 have a great day😍💖` }
         )
     }
 }
