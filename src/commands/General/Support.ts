202da211e50da032426891c8e77d13eb870f64a5
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-16 18:29:36 +0530

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
            MessageType.text
         ))
         const n = [
-            'https://telegra.ph/file/38a9538ae1f2387235caf.mp4','https://c.tenor.com/53rodm0H7VoAAAPo/kitagawa-kitagawa-marin.mp4'
+            'https://telegra.ph/file/38a9538ae1f2387235caf.mp4','https://telegra.ph/file/2a273f3ff694886c27de2.mp4'
         ]
         let beckylynch = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
