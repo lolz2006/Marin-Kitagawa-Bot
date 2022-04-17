f9c5472049fba64f5a1dd9fca4dc5cf9b9badae9
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-17 09:18:06 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "𝙕𝙀𝙍𝙊❤️ ",
-						author: pack[2] || "𝘼𝙆𝙐𝙈𝘼",
+						pack: pack[1] || "𝐙𝐄𝐑𝐎❤️ ",
+						author: pack[2] || "𝐀𝐊𝐔𝐌𝐀",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
